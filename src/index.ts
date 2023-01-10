8ce262d897279c9a5aedb169ab47a79879c4c3f6
Sunnie Kapar <kaparsunnie@gmail.com>
2023-01-09 20:53:59 -0500

    modal modal modal

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("your mom");
+const form = document.getElementById("form") as HTMLFormElement | null
+const input = document.getElementById("option1") as HTMLInputElement | null
+
+form?.addEventListener("submit", e => {
+    console.log("works")
+    e.preventDefault()
+
+    if(input?.value == "" || input?.value == null) {
+        console.log("your mom")
+    }
+
+})
+
