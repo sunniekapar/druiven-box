b24fbd79ce2947d77c0ae5382789e2ffe06aee3a
Sunnie Kapar <kaparsunnie@gmail.com>
2023-01-10 17:50:45 -0500

    wowowowow!

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
-const form = document.getElementById("form") as HTMLFormElement | null
-const input = document.getElementById("option1") as HTMLInputElement | null
+const inputSwitches = Array.from(document.querySelectorAll<HTMLInputElement>(".checkbox")) // inputs
+const output = document.querySelector(".output") as HTMLParagraphElement // output text
 
-form?.addEventListener("submit", e => {
-    console.log("works")
-    e.preventDefault()
+let inputSwitchesValue: number[] = [0,0,0,0] // all zero since all switches are not checked loaded in
 
-    if(input?.value == "" || input?.value == null) {
-        console.log("your mom")
+// checks if the answer is right
+function checkAnswer(a:number, b:number, c:number, d:number) {
+    if(a == 1 && b == 1) {
+       output.innerHTML = "1"
+       return
     }
+    output.innerHTML = "0"
+}
+changeValues()
+// updates the value after a switch is pressed
+function changeValues() {
+    for(let i = 0; i < inputSwitchesValue.length; i++) {
+        inputSwitchesValue[i] = Number(inputSwitches.at(i)?.value)
+    }
+    checkAnswer(inputSwitchesValue[0], inputSwitchesValue[1], inputSwitchesValue[2], inputSwitchesValue[3])
+}
 
+// changes the value of each switch
+inputSwitches.forEach(inputSwitch => {
+    inputSwitch.addEventListener('click', () => { 
+        inputSwitch.value = String(Math.abs(Number(inputSwitch.value) - 1))
+        changeValues()
+    })
 })
+
+
 
